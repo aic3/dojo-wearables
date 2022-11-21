bc5ba1a14a885a3e66607b471c910154daaff222
Malcolm Hyson <malcolmhyson@outlook.com>
2022-11-21 01:13:48 -0500

    adding Azure tables to user settings

--- src/dojo-attrib-svc/dojo-user-attrib/DojoSettings.cs
+++ src/dojo-attrib-svc/dojo-user-attrib/DojoSettings.cs
@@ -7,18 +7,30 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Data.Tables;
+using Azure.Data.Tables.Models;
+using Azure;
+using System.Linq;
+using System;
 
 namespace DevOpsDojo.Users.Functions
 {
     public class DojoSettings
     {
         private readonly ILogger<DojoSettings> _logger;
+        private IConfigurationRoot _config;
+        private static string SETTINGS_CONNECTION_STRING = "SETTINGS_CONNECTION_STRING";
+        private static string SETTINGS_USER_SETTINGS_TABLE = "usersettings";
 
         public DojoSettings(ILogger<DojoSettings> log)
         {
             _logger = log;
+
+            // load the local app settings
+            this.LoadAppSettings();
         }
 
         /// <summary>
@@ -44,26 +56,75 @@ namespace DevOpsDojo.Users.Functions
         /// </summary>
         /// <param name="req"></param>
         /// <returns></returns>
-        [FunctionName("UserSettings")]
-        public async Task<IActionResult> GetUserSettings(
+        [FunctionName("GetUserSettings")]
+        public async Task<IActionResult> GetUserSettingsFunc(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req){
             string body = await new StreamReader(req.Body).ReadToEndAsync();
-            
+
             // get the settings request
             UserSettings settingsInput = JsonConvert.DeserializeObject<UserSettings>(body);
 
             // get the id
-            string id = settingsInput.Id;
-
-            UserSettings settings = new UserSettings()
-            {
-                Id = settingsInput.Id + " output",
-                Name = "Sample user",
-                Shirt = "red",
-                Belt = "dojo-gi"
+            Guid id = settingsInput.Id;
+            UserSettings settings = new UserSettings() { 
+                Id = id
             };
-            // string 
+            TableClient tableClient = this.GetTableServiceClient();
+            Pageable<UserSettings> qryResults = tableClient.Query<UserSettings>(s => s.Id == id);
+
+            settings = qryResults.FirstOrDefault();
+            
             return new OkObjectResult(settings);
+        }
+
+        [FunctionName("SetUserSettings")]
+        public async Task<IActionResult> SetUserSettingsFunc(
+           [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
+        {
+            string body = await new StreamReader(req.Body).ReadToEndAsync();
+
+            // get the settings request
+            UserSettings settings = JsonConvert.DeserializeObject<UserSettings>(body);
+
+            TableClient tableClient = this.GetTableServiceClient();
+            Pageable<UserSettings> qryResults = tableClient.Query<UserSettings>(s => s.Id == settings.Id);
+
+            if (qryResults.Count() == 1)
+            {
+                UserSettings userSettings = qryResults.FirstOrDefault();
+                settings.ETag = userSettings.ETag;
+                settings.PartitionKey = userSettings.PartitionKey;
+                settings.RowKey = userSettings.RowKey;
+            }
+            else
+            {
+                settings.RowKey = settings.Id.ToString();
+                settings.PartitionKey = settings.Name.ToLower().Substring(0,1);                
+            }
+
+            settings.Timestamp = DateTime.UtcNow;
+
+            // retrieve the new updated
+            tableClient.UpsertEntity<UserSettings>(settings);
+
+            return new OkObjectResult(settings);
+        }
+
+        private TableClient GetTableServiceClient()
+        {
+            string connectionString = this._config[DojoSettings.SETTINGS_CONNECTION_STRING];
+
+            // ref: https://github.com/Azure/azure-sdk-for-net/tree/main/sdk/tables/Azure.Data.Tables/samples
+            // get the current users settings
+            return new TableClient(connectionString, DojoSettings.SETTINGS_USER_SETTINGS_TABLE);
+        }
+
+        private void LoadAppSettings()
+        {
+            this._config = new ConfigurationBuilder()
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
         }
     }
 }
--- src/dojo-attrib-svc/dojo-user-attrib/Settings/UserSettings.cs
+++ src/dojo-attrib-svc/dojo-user-attrib/Settings/UserSettings.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Azure;
+using Azure.Data.Tables;
+using Newtonsoft.Json;
 using System;
 
 namespace DevOpsDojo.Users.Functions.Settings
@@ -7,15 +9,23 @@ namespace DevOpsDojo.Users.Functions.Settings
     /// Dojo user settings class
     /// </summary>
     [Serializable]
-    public class UserSettings
+    public class UserSettings: ITableEntity
     {
         [JsonProperty("id")]
-        public string? Id { get; set; }
+        public Guid Id { get; set; }
         [JsonProperty("name")]
         public string? Name { get; set; }
         [JsonProperty("shirt")]
         public string? Shirt { get; set; }
         [JsonProperty("belt")]
-        public string Belt { get; set; }
+        public string? Belt { get; set; }
+        [JsonIgnore]
+        public string PartitionKey { get; set; }
+        [JsonIgnore]
+        public string RowKey { get; set; }
+        [JsonIgnore]
+        public DateTimeOffset? Timestamp { get; set; }
+        [JsonIgnore]
+        public ETag ETag { get; set; }
     }
 }
